0251313bc08a7ece866e1dde280a554006f21bf8
AhmadHoseiny <ahmadshahhat9898@gmail.com>
2023-03-15 16:11:10 +0200

    Trial Setups 2.0

--- src/DBApp.java
+++ src/DBApp.java
@@ -1,6 +1,7 @@
 public class DBApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
+        System.out.println("Once Again I'm trying");
     }
 }
