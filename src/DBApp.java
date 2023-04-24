75e66efe377c9fa7ccf77c656fc2e6bfcec2988f
Omar Wael <omarwaelmahmoud@gmail.com>
2023-04-24 02:48:43 +0200

    Updated Exception Handling

--- src/DBApp.java
+++ src/DBApp.java
@@ -38,24 +38,27 @@ public class DBApp {
 
 
         DBApp dbApp = new DBApp();
-        dbApp.createTable("Student", "id", htblColNameType,
-                htblColNameMin, htblColNameMax);
+//        dbApp.createTable("Student", "id", htblColNameType,
+//                htblColNameMin, htblColNameMax);
 
 /*
-        6
+        7
         1 AhmedNoor 0.95
         3 AhmedOmar 1.95
         2 AhmedAli 2.95
         5 Sayed 3.95
         4 Logine 4.95
         7 Omar 4.0
+        11 Abdelrahman 0.9
 */
+
+        Hashtable<String, Object> htblColNameValue;
 
 //        Scanner sc = new Scanner(System.in);
 //
 //        int n = sc.nextInt();
 //        for(int i=0 ; i<n ; i++){
-//            Hashtable<String, Object> htblColNameValue = new Hashtable( );
+//            htblColNameValue = new Hashtable( );
 //            int id = sc.nextInt();
 //            String name = sc.next();
 //            double gpa = sc.nextDouble();
@@ -65,14 +68,6 @@ public class DBApp {
 //            dbApp.insertIntoTable("Student", htblColNameValue);
 //        }
 
-//        Hashtable<String, Object> htblColNameValue = new Hashtable( );
-//        htblColNameValue.put("id", "11");
-//        htblColNameValue.put("names", "abdelrahman");
-//        htblColNameValue.put("gpa", 0.9);
-//        dbApp.insertIntoTable("Student", htblColNameValue);
-
-
-//        Hashtable htblColNameValue;
 //        htblColNameValue = new Hashtable( );
 //        htblColNameValue.put("gpas", 0.95);
 //        dbApp.updateTable("Student", "4", htblColNameValue);
@@ -85,8 +80,27 @@ public class DBApp {
 //        dbApp.deleteFromTable("Student", htblColNameValue);
 
 
-//        dbApp.printTable("Student");
+        dbApp.printTable("Student");
 //
+/*
+        3
+        id > 1 2
+        name = 3 AhmedNoor
+        gpa < 2 0.95
+        AND
+        OR
+
+        -----------------
+        4
+        id >= 1 4
+        gpa > 2 1.0
+        gpa < 2 4.0
+        id <= 1 7
+        AND
+        OR
+        AND
+        -----------------
+ */
 //        Scanner sc = new Scanner(System.in);
 //        int n = sc.nextInt();
 //        SQLTerm[] arrSQLTerms = new SQLTerm[n];
@@ -129,46 +143,52 @@ public class DBApp {
                             Hashtable<String,String> htblColNameType,
                             Hashtable<String,String> htblColNameMin,
                             Hashtable<String,String> htblColNameMax )
-            throws DBAppException, IOException {
-        Table t = new Table(strTableName, strClusteringKeyColumn, htblColNameType, htblColNameMin, htblColNameMax);
-        Serializer.serializeTable(t, strTableName);
+            throws DBAppException {
+
+        try {
+            Table t = new Table(strTableName, strClusteringKeyColumn, htblColNameType, htblColNameMin, htblColNameMax);
+            Serializer.serializeTable(t, strTableName);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
     public void insertIntoTable(String strTableName,
                                 Hashtable<String,Object> htblColNameValue)
-            throws DBAppException, IOException, ClassNotFoundException {
-        Table t = Serializer.deserializeTable(strTableName);
+            throws DBAppException {
         try {
+            Table t = Serializer.deserializeTable(strTableName);
             t.insertTuple(htblColNameValue);
-        } catch (DBAppException e) {
+            Serializer.serializeTable(t, strTableName);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        Serializer.serializeTable(t, strTableName);
     }
 
     public void updateTable(String strTableName,
                             String strClusteringKeyValue,
                             Hashtable<String,Object> htblColNameValue )
-            throws DBAppException, IOException, ClassNotFoundException {
-        Table t = Serializer.deserializeTable(strTableName);
+            throws DBAppException {
         try {
+            Table t = Serializer.deserializeTable(strTableName);
             t.updateTuple(strClusteringKeyValue, htblColNameValue);
-        } catch (DBAppException e) {
+            Serializer.serializeTable(t, strTableName);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        Serializer.serializeTable(t, strTableName);
     }
 
     public void deleteFromTable(String strTableName,
                                 Hashtable<String,Object> htblColNameValue)
-            throws DBAppException, IOException, ClassNotFoundException {
-        Table t = Serializer.deserializeTable(strTableName);
+            throws DBAppException {
         try {
+            Table t = Serializer.deserializeTable(strTableName);
             t.deleteTuple(htblColNameValue);
-        } catch (DBAppException e) {
+            Serializer.serializeTable(t, strTableName);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        Serializer.serializeTable(t, strTableName);
     }
 
     public void printTable(String strTableName) throws DBAppException, IOException, ClassNotFoundException {
